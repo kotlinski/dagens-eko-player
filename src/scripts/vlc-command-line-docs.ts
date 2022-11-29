0afdcb3c50fd7d8c4ba4d6ad68a39e0f5c6b22cb
Simon Kotlinski <simon.kotlinski@gmail.com>
2022-11-29 12:27:23 +0100

    chore(vlc-script): close process after print out

--- src/scripts/vlc-command-line-docs.ts
+++ src/scripts/vlc-command-line-docs.ts
@@ -1,9 +1,11 @@
 import ProcessorProvider from '../player/processor-provider';
 import SverigesRadioApiClient from '../sveriges-radio/sveriges-radio-api-client';
 
-const pp = new ProcessorProvider(new SverigesRadioApiClient());
-
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
-  await pp.printProcessorCommands();
+  const processor = new ProcessorProvider(new SverigesRadioApiClient());
+  await processor.printProcessorCommands();
+  setTimeout(() => {
+    processor.resetProcessor();
+  }, 1_000);
 })();
