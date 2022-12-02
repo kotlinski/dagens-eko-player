7e7d11be7e846c8292eb4a3d358b0d8975c18d9b
Simon Kotlinski <simon.kotlinski@gmail.com>
2022-12-03 00:59:22 +0100

    chore(sr-api-client): add api response interfaces (#49)

--- src/sveriges-radio/sveriges-radio-api-client.ts
+++ src/sveriges-radio/sveriges-radio-api-client.ts
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
-import { Episode, Episodes } from './interfaces';
+import { EpisodesApiResponse } from './api-interfaces';
 
 export default class SverigesRadioApiClient {
-  public async fetchEpisodes(program_id: string, number_of_episodes: number): Promise<Episode[]> {
+  public async fetchEpisodes(program_id: string, number_of_episodes: number): Promise<EpisodesApiResponse> {
     const url = `https://api.sr.se/api/v2/episodes/index?programid=${program_id}&audioquality=hi&format=json&size=${number_of_episodes}`;
     const response = await fetch(url);
-    return ((await response.json()) as Episodes).episodes;
+    return (await response.json()) as EpisodesApiResponse;
   }
 }
