4a457abee971fca774850f96445e44369bbc1388
Simon Kotlinski <simon.kotlinski@gmail.com>
2022-12-11 00:43:43 +0100

    chore(start-up): clean up script

--- src/__tests__/radio-starter.test.ts
+++ src/__tests__/radio-starter.test.ts
@@ -2,7 +2,6 @@ import Player from '../radio/player';
 import { bootRadio, killRadio } from '../radio-starter';
 
 jest.mock('../radio/player');
-
 describe('bootRadio', () => {
   it('should have created a player', () => {
     const radio = bootRadio();
