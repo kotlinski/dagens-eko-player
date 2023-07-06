004c621bc7c846d3e7076a5787017eedad45c9f4
Simon Kotlinski <simon.koltlinski@gmail.com>
2023-07-06 12:24:35 +0200

    feature(episodes-notifier): start with radio

--- src/__tests__/radio-starter.test.ts
+++ src/__tests__/radio-starter.test.ts
@@ -3,10 +3,13 @@ import { bootRadio, killRadio } from '../radio-starter';
 
 jest.mock('../radio/player');
 describe('bootRadio', () => {
-  it('should have created a player', () => {
-    const radio = bootRadio();
-    expect(Player).toHaveBeenCalledTimes(1);
-    radio.kill();
+  let radio: Player;
+  afterAll(() => {
     killRadio();
+    radio.kill();
+  });
+  it('should have created a player', () => {
+    radio = bootRadio();
+    expect(Player).toHaveBeenCalledTimes(1);
   });
 });
